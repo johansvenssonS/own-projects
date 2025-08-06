4973dd2128fd330193e5bdce19538f4eec6fefbd
Johan Svensson <johan.svensson0608@gmail.com>
2025-08-06 22:32:19 +0200

    första

---
+++ portfolio/src/main.js
@@ -0,0 +1,32 @@
+///import OBJEKT from './object.js'; 
+// customElements.define("component-name", OBJEKT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///NAVIGATION
+import Router from "./router.js";
+customElements.define("router-component", Router);
+
+import Navigation from "./navigation.js";
+customElements.define("navigation-component", Navigation);
---
+++ portfolio/src/router.js
@@ -0,0 +1,36 @@
+export default class Router extends HTMLElement {
+  constructor() {
+      super();
+
+      this.currentRoute = "";
+
+      this.allRoutes = {
+          "": {
+              view: "<start-view></start-view>",
+              name: "start",
+          },
+          "project": {
+              view: "<project-view></project-view>",
+              name: "Projekt",
+          }
+  }     
+}
+
+  get routes() {
+      return this.allRoutes;
+  }
+
+    // connect component
+    connectedCallback() {
+        window.addEventListener('hashchange', () => {
+            this.resolveRoute();
+        });
+
+        this.resolveRoute();
+    }
+
+    resolveRoute() {
+        const newRoute = location.hash.replace("#", "");
+        this.currentRoute = newRoute;
+    }
+  }
