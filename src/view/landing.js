447329c4a6a273a286a4b09094a11eefecd6cd99
johansvenssonS <johan.svensson0608@gmail.com>
2025-08-27 16:48:42 +0200

    Delete src directory
    överflödigt

--- src/view/landing.js
+++
@@ -1,9 +0,0 @@
-export default class Landing extends HTMLElement {
-  // connect component
-  connectedCallback() {
-      this.innerHTML =    `
-                           <projects-component></projects-component>
-
-                           `;
-  }
-}
